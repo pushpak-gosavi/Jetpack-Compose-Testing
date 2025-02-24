6b0f665793e66a245f69ea71f6e0180a159762c1
Pushpak Gosavi <pushpak.gosavi7@gmail.com>
2025-02-24 16:38:02 +0530

    master:Stable, Initial commit

---
+++ app/src/main/java/com/pushpak/textfield/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.pushpak.textfield
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.pushpak.textfield.ui.theme.TextFieldTheme
+import com.pushpak.textfield.viewmodel.MainViewModel
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContent {
+            TextFieldTheme {
+                val mainViewModel: MainViewModel = viewModel()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Cryprograpicdata(
+                        modifier = Modifier.padding(innerPadding),
+                        mainViewModel = mainViewModel
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Cryprograpicdata(modifier: Modifier, mainViewModel: MainViewModel) {
+    var textFieldValue by rememberSaveable  { mutableStateOf("") }
+    val scope = rememberCoroutineScope()
+    var flowData by remember { mutableStateOf("") }
+    Column(
+        modifier = modifier
+    ) {
+        TextField(
+            value = textFieldValue,
+            onValueChange = { textFieldValue = it }
+        )
+        Spacer(Modifier.height(10.dp))
+        Button(
+            onClick = {
+                mainViewModel.setDataToCryptoText(textFieldValue)
+                scope.launch {
+                    mainViewModel.flowData().collect{
+                        flowData = it.toString()
+                    }
+                }
+
+
+            }
+        ) {
+            Text(text = "Click")
+        }
+        Spacer(Modifier.height(10.dp))
+        mainViewModel.getData().observeAsState().value?.let { Text(text = it) }
+        Spacer(Modifier.height(10.dp))
+        Text(flowData)
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TextFieldTheme {
+        Cryprograpicdata(modifier = Modifier.padding(8.dp), MainViewModel())
+    }
+
+}
---
+++ app/src/main/java/com/pushpak/textfield/viewmodel/MainViewModel.kt
@@ -0,0 +1,31 @@
+package com.pushpak.textfield.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    private var cryptoText = MutableLiveData("")
+    fun setDataToCryptoText(text: String) {
+        viewModelScope.launch {
+            cryptoText.postValue(text)
+        }
+    }
+
+    fun getData(): MutableLiveData<String> {
+        return cryptoText
+    }
+
+    fun flowData(): Flow<Int> {
+        return flow<Int> {
+            for (i in 1..5) {
+                delay(1000)
+                emit(i)
+            }
+        }
+    }
+}
